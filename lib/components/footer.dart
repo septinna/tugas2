247b1212d97697b033b7a3a3d413d1ac434ba6cd
septinna <129613418+septinna@users.noreply.github.com>
2025-04-25 00:20:16 +0700

    Update footer.dart

--- lib/components/footer.dart
+++ lib/components/footer.dart
@@ -3,18 +3,28 @@ import 'package:flutter/material.dart';
 class Footer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(15),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.facebook, size: 20),
-          SizedBox(width: 15),
-          Icon(Icons.email, size: 20),
-          SizedBox(width: 15),
-          Icon(Icons.link, size: 20),
-        ],
-      ),
+    return Column(
+      children: [
+        Divider(
+          color: const Color.fromARGB(66, 0, 0, 0),
+          thickness: 5,
+          indent: MediaQuery.of(context).size.width * 0.1, // 10% of screen width
+          endIndent: MediaQuery.of(context).size.width * 0.1, // 10% of screen width
+        ),
+        Container(
+          padding: EdgeInsets.all(15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.facebook, size: 20),
+              SizedBox(width: 15),
+              Icon(Icons.email, size: 20),
+              SizedBox(width: 15),
+              Icon(Icons.link, size: 20),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
